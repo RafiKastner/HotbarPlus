2f2ec2e95892fc751c2f7b47c150285a68286823
RafiKastner <rafikastner@gmail.com>
2024-10-18 12:44:41 -0400

    Org name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,6 +14,7 @@ const config = {
 
   url: 'https://rafikastner.github.io/HotbarPlus/',
   baseUrl: '/',
+  organizationName: 'RafiKastner',
   projectName: 'HotbarPlus',
 
   onBrokenLinks: 'throw',
