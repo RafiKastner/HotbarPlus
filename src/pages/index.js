46ced3cff0c3acee48f7032ad9b73d68b17017a0
RafiKastner <rafikastner@gmail.com>
2024-10-20 17:16:39 -0400

    Fixed broken links

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro/tutorial">
+            to="/docs/intro/Tutorial">
             HotbarPlus Tutorial - 5min ⏱️
           </Link>
         </div>
