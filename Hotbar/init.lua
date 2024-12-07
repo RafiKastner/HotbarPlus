6c6b6b00939192149f7f27f874ae5ac7fdd16c5c
Rafi Kastner <149256857+RafiKastner@users.noreply.github.com>
2024-12-07 14:53:48 -0500

    Bugfixes: resetPressAgain now works, fixed incorrect syntax

--- Hotbar/init.lua
+++ Hotbar/init.lua
@@ -118,13 +118,13 @@ end)
 function Hotbar.new()
 	local self = {}
 	setmetatable(self, Hotbar)
-	
+
 	--Janitors
 	local janitor = Janitor.new()
 	self.janitor = janitor
 	self.captionJanitor = janitor:Add(Janitor.new())
 	self.oneClickJanitor = janitor:Add(Janitor.new())
-	
+
 	-- Register
 	local UID
 	local function UIDgen()
@@ -139,7 +139,7 @@ function Hotbar.new()
 	janitor:Add(function()
 		hotbarDict[UID] = nil
 	end)
-	
+
 	-- Signals
 	self.selected = janitor:Add(Signal.new())
 	self.deselected = janitor:Add(Signal.new())
@@ -151,7 +151,7 @@ function Hotbar.new()
 	self.viewingStarted = janitor:Add(Signal.new())
 	self.viewingEnded = janitor:Add(Signal.new())
 	self.toggleKeyAdded = janitor:Add(Signal.new())
-	
+
 	-- Properties
 	self.hotbarModule = module
 	self.isSelected = false
@@ -177,20 +177,20 @@ function Hotbar.new()
 	self.boundPressAgain = {}
 	self.boundToggleKeys = {}
 	self.boundToggleItems = {}
-	
+
 	--Widget is the name for the gui (item is the item/slot itself)
 	local holder, widget = require(script.Elements.Widget)(self, Hotbar)
 	self.widget = widget
 	self.holder = holder
 	self.widgetOutline = self.widget.UIStroke
 	self.widgetFill = self.widget.Fill
-		
+
 	-- Internal Functions/Listeners
 	local function handleToggle()
 		if self.isLocked then
 			return
 		end
-		
+
 		if self.isOnCooldown and not self.canSelectOnCooldown and not self.shouldDeselectOnCooldown and not self.isSelected then
 			return
 		end
@@ -203,55 +203,55 @@ function Hotbar.new()
 		end
 		return self
 	end
-	
+
 	local function viewingStarted(dontSetState)
 		if self.isLocked or self.isOnCooldown then
 			return
 		end
-		
+
 		self.isViewing = true
 		self.viewingStarted:Fire()
 		if not dontSetState then
 			self:setState("Viewing", "User", self)
 		end
 	end
-	
+
 	local function viewingEnded()
 		if self.isLocked or self.isOnCooldown then 
 			return
 		end
-		
+
 		self.isViewing = true
 		self.viewingEnded:Fire()
 		self:setState(nil, "User", self)
 	end
-	
+
 	local function oneClickUsingRelease()
 		if self.isHoldingOneClickUse then
 			self.isHoldingOneClickUse = false
 			self:attemptTo("endUse")
 		end
 	end
-	
+
 	widget.TextButton.MouseButton1Down:Connect(handleToggle)
 	widget.TextButton.MouseButton1Up:Connect(oneClickUsingRelease)
 	widget.TouchTap:Connect(handleToggle)
-	
+
 	widget.TextButton.MouseEnter:Connect(function()
 		local dontSetState = not UserInputService.KeyboardEnabled
 		viewingStarted(dontSetState)
 	end)
-	
+
 	widget.TextButton.MouseLeave:Connect(function()
 		viewingEnded()
 	end)
-	
-	
+
+
 	local function mouseInput(input)		
 		if self.isLocked then
 			return
 		end
-		
+
 		local pass = not self.shouldEndUseOnCooldown and self.isUsing
 		local pressNum = self.pressAgainNum
 		local pressAgainPass = pressNum <= self.pressAgainMax and pressNum ~= 0
@@ -281,51 +281,51 @@ function Hotbar.new()
 				self:attemptTo("endUse")
 			end
 		end
-		
+
 	end
-	
+
 	janitor:Add(UserInputService.InputBegan:Connect(function(input, GPE)
 		if GPE then
 			return
 		end
-		
+
 		if self.boundToggleKeys[input.KeyCode] then
 			handleToggle()
 		end
 		mouseInput(input)
 	end))
-	
+
 	janitor:Add(UserInputService.InputEnded:Connect(function(input, GPE)
 		if GPE then
 			return
 		end
-		
+
 		if self.boundToggleKeys[input.KeyCode] and self.isHoldingOneClickUse then
 			self.isHoldingOneClickUse = false
 			self:attemptTo("endUse")
 		end
-		
+
 		mouseInput(input)
 	end))
-		
+
 	self.selected:Connect(function()
 		if self.outlineEnabled then
 			self.widgetOutline.Enabled = true
 		end
 	end)
-	
+
 	self.deselected:Connect(function()
 		if self.outlineEnabled then
 			self.widgetOutline.Enabled = false
 		end
 	end)
-	
+
 	janitor:Add(anyItemSelected:Connect(function(incomingItem)
 		if incomingItem ~= self and self.autoDeselect and incomingItem.autoDeselect then
 			self:deselect("AutoDeselect", incomingItem)
 		end
 	end))
-	
+
 	return self
 end
 
@@ -524,7 +524,7 @@ function Hotbar:setWidgetFill(percentFill, duration)
 	local maxFillSize = self.widget.TextButton.size.Y.Offset --this is dumb; change to to be based off theme when adding that
 	local fill = percentFill * .01 * maxFillSize
 	local udimOld = self.widgetFill.Size
-	
+
 	self.widgetFill.Size = UDim2.new(1, 0, 0, fill) --also lowkey dumb hardcodes but idc
 	if duration then
 		local tweenInfo = TweenInfo.new(duration, Enum.EasingStyle.Linear) 
@@ -541,6 +541,9 @@ function Hotbar:cooldown(duration)
 	end
 	if self.shouldEndUseOnCooldown then
 		self:endUse()
+	end
+	if self.resetPressAgain then
+		self.pressAgainNum = 0
 	end
 	self.isOnCooldown = true
 	local tween = self:setWidgetFill(100, duration)
@@ -622,21 +625,21 @@ function Hotbar:endUse()
 	return self
 end
 
-function Hotbar:destroy() {
+function Hotbar:destroy()
 	self.janitor:Cleanup()
 	self.holder:Destroy() --may need "DisconnectAll" after each goodsignal added to janitor
-}
+end
 
 function Hotbar:setState(incomingState, fromSource, sourceItem)	
 	if incomingState == nil then
 		incomingState = self.isSelected and "selected" or "deselected"
 	end
-	
+
 	local currentState = self.activeState
 	if incomingState == currentState then
 		return
 	end
-	
+
 	self.activeState = incomingState
 	local selected = self.isSelected
 	if incomingState == "selected" then
