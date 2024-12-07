219023c46e2f3d058b0abc75698f93ad777f65b5
RafiKastner <rafikastner@gmail.com>
2024-12-07 17:24:35 -0500

    Added gifs & images to home page

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,11 +1,14 @@
 import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
+import Hotbar_EasyToUse from '@site/static/gif/Hotbar_EasyToUse.gif';
+import Hotbar_ObjectOriented from '@site/static/gif/Hotbar_ObjectOriented.gif';
+import Hotbar_Lightweight from '@site/static/img/Hotbar_Lightweight.png';
 
 const FeatureList = [
   {
     title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    Img: Hotbar_EasyToUse,
     description: (
       <>
         HotbarPlus uses simple and dynamic methods that you can call
@@ -15,7 +18,7 @@ const FeatureList = [
   },
   {
     title: 'Lightweight',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    Img: Hotbar_Lightweight,
     description: (
       <>
         Streamlined and efficient with GoodSignal and Janitor, HotbarPlus
@@ -25,7 +28,7 @@ const FeatureList = [
   },
   {
     title: 'Object-Oriented',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    Img: Hotbar_ObjectOriented,
     description: (
       <>
         Not only easy to use with default methods, but intuitive to modify
@@ -35,11 +38,11 @@ const FeatureList = [
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({Img, title, description}) {
   return (
     <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+      <div className={styles.imgContainer + " text--center"}>
+        <img className={styles.featureSvg} role="img" src={Img}/>
       </div>
       <div className="text--center padding-horiz--md">
         <Heading as="h3">{title}</Heading>
